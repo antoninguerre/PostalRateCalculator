f50e856293a055490c9694263b1b55aaf7bf0863
Antonin Guerre <antonin.guerre@mail.mcgill.ca>
2021-10-26 19:50:32 -0400

    Created main page (in progress)

--- PostalRateCalculator-Frontend/src/main.js
+++ PostalRateCalculator-Frontend/src/main.js
@@ -1,13 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import BootstrapVue from "bootstrap-vue"
+import VueMaterial from 'vue-material'
 import App from './App'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/black-green-light.css'
 
-Vue.use(BootstrapVue)
+Vue.use(VueMaterial)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- PostalRateCalculator-Frontend/src/router/index.js
+++ PostalRateCalculator-Frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import PostalRateCalculator from "../components/PostalRateCalculator";
+import Test from "../components/Test";
 
 Vue.use(Router)
 
@@ -8,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'PostalRateCalculator',
+      component: PostalRateCalculator
     }
   ]
 })
