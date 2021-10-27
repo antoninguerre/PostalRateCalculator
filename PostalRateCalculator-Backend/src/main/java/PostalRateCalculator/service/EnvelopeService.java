9c273f375e7a74c7f38921a231bca7509fe4bb45
Antonin Guerre <antonin.guerre@mail.mcgill.ca>
2021-10-27 13:41:24 -0400

    Started adding code in the service method

--- PostalRateCalculator-Backend/src/main/java/PostalRateCalculator/service/EnvelopeService.java
+++ PostalRateCalculator-Backend/src/main/java/PostalRateCalculator/service/EnvelopeService.java
@@ -2,9 +2,12 @@ package PostalRateCalculator.service;
 
 import PostalRateCalculator.dao.EnvelopeRepository;
 import PostalRateCalculator.model.Envelope;
+import PostalRateCalculator.model.SizeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class EnvelopeService {
@@ -14,6 +17,12 @@ public class EnvelopeService {
 
     @Transactional
     public int calculatePostalRate(Envelope envelope) {
+
+        // Verify that the envelope's width is not smaller than 140mm if the size unit is in millimeters
+        if (envelope.getSizeUnit() == SizeUnit.Millimeters && envelope.getWidth() < 140) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The envelope is not wide enough.");
+        }
+
         return 0;
     }
 }
